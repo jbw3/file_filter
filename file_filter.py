6f3d659bccdcba02f639c3f880f53114c17b71c9
John Wilkes <jwilkes0011@gmail.com>
2022-07-22 17:19:47 -0500

    Starting row filtering

--- file_filter.py
+++ file_filter.py
@@ -3,15 +3,30 @@
 import argparse
 import sys
 
+class Row:
+    def __init__(self, values):
+        self._values = values
+
+    def __len__(self):
+        return len(self._values)
+
+    def __getitem__(self, key):
+        return self._values[key]
+
 def filter_file(args, inFile, outFile):
     if args.filter is None:
-        filter_row = lambda l: True
+        filter_row = lambda l, r: True
     else:
-        filter_row = lambda l: eval(args.filter)
+        filter_row = lambda l, r: eval(args.filter)
+
+    header = inFile.readline()
+    outFile.write(header)
 
     for line in inFile:
-        if filter_row(line):
-            print(line, end='', file=outFile)
+        split = line.rstrip('\r\n').split(',')
+        row = Row(split)
+        if filter_row(line, row):
+            outFile.write(line)
 
 def parse_args():
     parser = argparse.ArgumentParser()
